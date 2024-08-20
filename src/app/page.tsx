47468397ecc5fbbbdfc283e9234c81a70bbd55cf
r0binsahin <robin.sahin88@gmail.com>
2024-08-20 15:30:46 +0200

    creates gauge
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image';
+import * as Components from '../components/index';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Survey form page</h1>
+      <Components.Gauge value={0.5} />
     </main>
   );
 }
--- src/components/gauge.tsx
+++ src/components/gauge.tsx
@@ -1,3 +1,38 @@
-export const Gauge = () => {
-  return <>Gauge</>;
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+interface GaugeProps {
+  value: number;
+}
+
+export const Gauge = ({ value }: GaugeProps) => {
+  const gaugeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (gaugeRef.current) {
+      const gaugeFill = gaugeRef.current.querySelector(
+        '.gauge__fill'
+      ) as HTMLDivElement;
+      const gaugeCover = gaugeRef.current.querySelector(
+        '.gauge__cover'
+      ) as HTMLDivElement;
+
+      if (value >= 0 && value <= 1) {
+        gaugeFill.style.transform = `rotate(${value / 2}turn)`;
+        gaugeCover.textContent = `${Math.round(value * 100)}%`;
+      }
+    }
+  }, [value]);
+
+  return (
+    <div className='gauge w-full max-w-xs text-[#004033] text-2xl font-roboto'>
+      <div className='gauge__body w-full h-0 pb-[50%] bg-[#b4c0be] relative rounded-t-[100%_200%] overflow-hidden'>
+        <div className='gauge__fill absolute top-full left-0 w-full h-full bg-[#009578] origin-[center_top] rotate-[0.25turn] transition-transform duration-200 ease-out'></div>
+        <div className='gauge__cover w-[75%] h-[150%] bg-white rounded-full absolute top-[25%] left-1/2 transform -translate-x-1/2 flex items-center justify-center pb-[25%] box-border'>
+          {value * 100}%
+        </div>
+      </div>
+    </div>
+  );
 };
