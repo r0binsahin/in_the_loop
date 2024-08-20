61b6d5f1cd943db7ec109c8a8b7e53fbc9d54b63
r0binsahin <robin.sahin88@gmail.com>
2024-08-20 13:25:51 +0200

    creates tables
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

---
+++ drizzle/0000_quick_storm.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS "answers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"rating" real NOT NULL,
+	"question_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "questions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"text" text NOT NULL,
+	"survey_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "surveys" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_amount" integer NOT NULL,
+	"date" date NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "answers" ADD CONSTRAINT "answers_question_id_questions_id_fk" FOREIGN KEY ("question_id") REFERENCES "public"."questions"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "questions" ADD CONSTRAINT "questions_survey_id_surveys_id_fk" FOREIGN KEY ("survey_id") REFERENCES "public"."surveys"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,26 @@
-import { pgTable, serial } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  integer,
+  text,
+  date,
+  real,
+} from 'drizzle-orm/pg-core';
 
-export const test = pgTable('test', {
+export const surveys = pgTable('surveys', {
   id: serial('id').primaryKey(),
+  user_amount: integer('user_amount').notNull(),
+  date: date('date').notNull(),
+});
+
+export const questions = pgTable('questions', {
+  id: serial('id').primaryKey(),
+  text: text('text').notNull(),
+  survey_id: integer('survey_id').references(() => surveys.id),
+});
+
+export const answers = pgTable('answers', {
+  id: serial('id').primaryKey(),
+  rating: real('rating').notNull(),
+  question_id: integer('question_id').references(() => questions.id),
 });
