63fe6403a2c56283de702416833e1eb16ade7d22
r0binsahin <robin.sahin88@gmail.com>
2024-08-20 15:38:58 +0200

    creates action function for questions
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,1 +1,11 @@
 'use server';
+
+import { queryGetQuestions } from '@/server/queries';
+
+export const getQuestions = async () => {
+  try {
+    return await queryGetQuestions();
+  } catch (error) {
+    console.error(error);
+  }
+};
