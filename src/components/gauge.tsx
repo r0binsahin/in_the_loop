c97666a6607259d83eec8ffd5787c45df262fe55
r0binsahin <robin.sahin88@gmail.com>
2024-08-21 14:08:29 +0200

    #19 gouge changes dynamicly
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/components/gauge.tsx
+++ src/components/gauge.tsx
@@ -1,39 +1,43 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
+const GaugeComponent = dynamic(() => import('react-gauge-component'), {
+  ssr: false,
+});
 
 interface GaugeProps {
   value: number;
 }
 
 export const Gauge = ({ value }: GaugeProps) => {
-  const gaugeRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (gaugeRef.current) {
-      const gaugeFill = gaugeRef.current.querySelector(
-        '.gauge__fill'
-      ) as HTMLDivElement;
-      const gaugeCover = gaugeRef.current.querySelector(
-        '.gauge__cover'
-      ) as HTMLDivElement;
-
-      if (value >= 0 && value <= 10) {
-        gaugeFill.style.transform = `rotate(${value / 2 + 0.25}turn)`;
-        gaugeCover.textContent = `${Math.round(value * 10)}%`;
-      }
-    }
-    console.log(value);
-  }, [value]);
-
   return (
-    <div className='gauge w-full max-w-xs text-[#004033] text-2xl font-roboto'>
-      <div className='gauge__body w-full h-0 pb-[50%] bg-[#b4c0be] relative rounded-t-[100%_200%] overflow-hidden'>
-        <div className='gauge__fill absolute top-full left-0 w-full h-full bg-[#009578] origin-[center_top] rotate-[0.25turn] transition-transform duration-200 ease-out'></div>
-        <div className='gauge__cover w-[75%] h-[150%] bg-white rounded-full absolute top-[25%] left-1/2 transform -translate-x-1/2 flex items-center justify-center pb-[25%] box-border'>
-          {Math.round(value * 10)}%
-        </div>
-      </div>
-    </div>
+    <GaugeComponent
+      arc={{
+        subArcs: [
+          {
+            limit: 20,
+            color: '#EA4228',
+            showTick: true,
+          },
+          {
+            limit: 40,
+            color: '#F58B19',
+            showTick: true,
+          },
+          {
+            limit: 60,
+            color: '#F5CD19',
+            showTick: true,
+          },
+          {
+            limit: 100,
+            color: '#5BE12C',
+            showTick: true,
+          },
+        ],
+      }}
+      value={value * 10}
+    />
   );
 };
