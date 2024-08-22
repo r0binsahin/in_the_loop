ee700e68ef176441f37900792ce6c47e12faf8a7
r0binsahin <robin.sahin88@gmail.com>
2024-08-22 14:12:56 +0200

    #29 ref: cleans survey component
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/components/survey-form.tsx
+++ src/components/survey-form.tsx
@@ -1,42 +1,13 @@
 'use client';
-import { useState } from 'react';
-import { Carousel, Slider } from './';
+
+import { Carousel } from './';
 
 import { Question } from '@/lib/types/Question';
-import { createAnswer } from '@/lib/actions';
-import { Answer } from '@/lib/types/Answer';
 
 interface SurveyFormProps {
   questions: Question[];
 }
 
 export const SurveyForm = ({ questions }: SurveyFormProps) => {
-  const [answers, setAnswers] = useState<Answer[]>([]);
-
-  const submitAnswers = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      if (answers.length === 0) console.log('no answers');
-
-      await Promise.all(
-        answers.map(async (answer) => {
-          await createAnswer(answer);
-        })
-      );
-
-      /*       console.log('Answers created:', answers.length);
-      console.log('Answers:', answers); */
-      setAnswers([]);
-    } catch (error) {
-      console.error('Error creating answers:', error);
-    }
-  };
-
-  return (
-    <>
-      <Carousel questions={questions} />
-      <button onClick={(e) => submitAnswers(e)}>Submit</button>
-    </>
-  );
+  return <Carousel questions={questions} />;
 };
