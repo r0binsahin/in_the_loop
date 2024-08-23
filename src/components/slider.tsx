09e53ea3ce1f55d4f224f1050d4f55f4fca5ce3a
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-23 11:28:58 +0200

    #35 Deletes unused imports from the Slider
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,9 +1,3 @@
-'use client';
-
-import { Answer } from '@/lib/types/Answer';
-import { answers } from '@/server/db/schema';
-import { SetStateAction, useState, MouseEvent } from 'react';
-
 interface SliderProps {
   value: number;
   setValue: (value: number) => void;
@@ -15,17 +9,17 @@ export const Slider = ({ value, setValue }: SliderProps) => {
   };
 
   return (
-    <div className='slidecontainer'>
-      <p className='mt-4 mb-2'>Custom range slider:</p>
+    <div className="slidecontainer">
+      <p className="mt-4 mb-2">Custom range slider:</p>
       <input
-        type='range'
-        min='1'
-        max='10'
+        type="range"
+        min="1"
+        max="10"
         value={value}
         onChange={handleSliderChange}
-        className='slider w-full h-4 bg-gray-300 rounded-lg appearance-none cursor-pointer'
+        className="slider w-full h-4 bg-gray-300 rounded-lg appearance-none cursor-pointer"
       />
-      <p className='mt-4'>Current value: {value}</p>
+      <p className="mt-4">Current value: {value}</p>
     </div>
   );
 };
