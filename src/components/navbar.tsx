bc4d246736f31562bf993df2988ed0b0a2c74de1
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-08-23 16:07:03 +0200

    #35 Adds styling to a navbar
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,38 @@
 "use client";
 
 import Link from "next/link";
+import { CgProfile } from "react-icons/cg";
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav className="flex justify-between my-5 py-5">
-        <div></div>
+    <div className="navbar bg-secondary">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div className="flex-1 justify-center">
         <Link href="/">
           <h1 className="font-black text-accent text-2xl">In the Loop</h1>
         </Link>
-        <Link href="/admin">login</Link>
-      </nav>
+      </div>
+      <div className="flex-none">
+        <Link href="/admin">
+          <CgProfile size={20} />
+        </Link>
+      </div>
     </div>
   );
 };
