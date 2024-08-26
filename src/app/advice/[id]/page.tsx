5e12f285cc127c99188f8c03cfd104e687fed151
r0binsahin <robin.sahin88@gmail.com>
2024-08-26 13:37:30 +0200

    #46 optimize advice fetch
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/advice/[id]/page.tsx
+++ src/app/advice/[id]/page.tsx
@@ -5,63 +5,47 @@ import {
   getQuestions,
   getRatingsByQuestionId,
 } from '@/lib/actions';
-import { Question } from '@/lib/types/Question';
 import { calculateAverageRatingPerQuestion } from '@/lib/utils/calculate-average-rating-per-question';
 
 import { useParams } from 'next/navigation';
-import { Suspense, useEffect, useState } from 'react';
-
-import React from 'react';
+import { useEffect, useState, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import { LoadingSkeleton } from '@/components';
 
-type AverageRatings = { [key: number]: number };
-
 export default function Advice() {
   const params = useParams<{ id: string }>();
-  const [question, setQuestion] = useState<Question | null>(null);
-  const [averageRatings, setAverageRatings] = useState<AverageRatings>({});
+
   const [advice, setAdvice] = useState<string | null>(null);
 
+  const hasFetchedAdvice = useRef(false);
+
   useEffect(() => {
-    const fetchQuestion = async () => {
+    const fetchData = async () => {
+      if (hasFetchedAdvice.current) return;
+
+      hasFetchedAdvice.current = true;
       const result = (await getQuestions()) || [];
       const foundQuestion = result.find((q) => q.id === +params.id);
-      setQuestion(foundQuestion!);
-    };
 
-    fetchQuestion();
-  }, [params.id]);
+      if (foundQuestion) {
+        const ratings = await getRatingsByQuestionId(+params.id);
+        const averageRating = calculateAverageRatingPerQuestion(ratings || []);
 
-  useEffect(() => {
-    const fetchRatings = async () => {
-      const newRatings: AverageRatings = {};
-      const ratings = await getRatingsByQuestionId(+params.id);
-      newRatings[+params.id] = calculateAverageRatingPerQuestion(ratings || []);
-      setAverageRatings(newRatings);
-    };
-    fetchRatings();
-  }, [params.id]);
+        const prompt = `Based on a survey conducted in our office, we scored a ${averageRating} out of 10 on this question: "${foundQuestion.text}". How can we improve this?`;
 
-  useEffect(() => {
-    if (question && Object.keys(averageRatings).length > 0) {
-      const rating = averageRatings[+params.id];
-      const prompt = `Based on a survey conducted in our office, we scored a ${rating} out of 10 on this question: ${question.text}. How can we improve this?`;
-
-      const fetchAdvice = async () => {
         const supportResponse = await generateAISupport(prompt);
         setAdvice(supportResponse.content);
-      };
+      }
+    };
 
-      fetchAdvice();
-    }
-  }, [question, averageRatings, params.id]);
+    fetchData();
+  }, [params.id]);
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Ai advice page</h1>
+      <h1>AI Advice Page</h1>
 
       {advice ? (
         <ReactMarkdown remarkPlugins={[remarkGfm]}>{advice}</ReactMarkdown>
--- src/lib/advice.ts
+++ src/lib/advice.ts
@@ -1,12 +1,10 @@
 'use server';
 
 import OpenAI from 'openai';
-import { Question } from './types/Question';
 
 const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export const giveAdvice = async (prompt: string) => {
-  console.log(prompt);
   const completion = await openAi.chat.completions.create({
     model: 'gpt-4o-mini',
     messages: [
@@ -21,7 +19,7 @@ export const giveAdvice = async (prompt: string) => {
       },
     ],
   });
-  console.log('AI answer', completion.choices[0].message);
+  console.log('AI answer', completion.choices[0].message.content);
 
   return completion.choices[0].message;
 };
