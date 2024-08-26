3203a6ace02b54bf3536a2faf9b9df0c2effe515
iiiamken <kenny.t.dinh93@gmail.com>
2024-08-26 16:03:56 +0200

    #49 finalized graph for survey

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client"
 import {
   LineChart,
   Line,
@@ -7,66 +7,21 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-} from 'recharts';
-const data = [
-  {
-    name: 'Jan',
-    Rating: 4,
-  },
-  {
-    name: 'Feb',
-    Rating: 6,
-  },
-  {
-    name: 'Mars',
-    Rating: 7,
-  },
-  {
-    name: 'April',
-    Rating: 4,
-  },
-  {
-    name: 'May',
-    Rating: 5,
-  },
-  {
-    name: 'June',
-    Rating: 8,
-  },
-  {
-    name: 'July',
-    Rating: 9,
-  },
-  {
-    name: 'Aug',
-    Rating: 9,
-  },
-  {
-    name: 'Sep',
-    Rating: 9,
-  },
-  {
-    name: 'Oct',
-    Rating: 9,
-  },
-  {
-    name: 'Nov',
-    Rating: 9,
-  },
-  {
-    name: 'Dec',
-    Rating: 9,
-  },
-];
-export default function Graph() {
+} from "recharts"
+
+export type GraphData = {
+  month: string
+  Rating: number
+}
+export default function Graph({ data }: { data: GraphData[] }) {
   return (
     <LineChart width={1000} height={600} data={data}>
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' padding={{ left: 30, right: 30 }} />
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" padding={{ left: 30, right: 30 }} />
       <YAxis domain={[0, 10]} ticks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />
       <Tooltip />
       <Legend />
-      <Line type='monotone' dataKey='Rating' stroke='#82CA9D' />
+      <Line type="monotone" dataKey="Rating" stroke="#82CA9D" />
     </LineChart>
-  );
+  )
 }
