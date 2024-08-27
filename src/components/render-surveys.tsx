13e35d8a1cd59f0a503f1e60e5a06a188c6758fb
r0binsahin <robin.sahin88@gmail.com>
2024-08-27 14:27:17 +0200

    #57 renders button conditionally
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/components/render-surveys.tsx
+++ src/components/render-surveys.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { createSurvey } from '@/lib/actions';
+import { createSurvey, getQuestionsBySurveyId } from '@/lib/actions';
+import { Question } from '@/lib/types/Question';
 import { Survey } from '@/lib/types/Survey';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -12,6 +13,10 @@ interface RenderSurveysProps {
 export const RenderSurveys = ({ surveys }: RenderSurveysProps) => {
   const [showForm, setShowForm] = useState(false);
   const [surveyName, setSurveyName] = useState('');
+  const [questionsMap, setQuestionsMap] = useState<Record<number, Question[]>>(
+    {}
+  );
+  const [activeSurveyId, setActiveSurveyId] = useState<number | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,6 +25,25 @@ export const RenderSurveys = ({ surveys }: RenderSurveysProps) => {
 
     setSurveyName('');
     setShowForm(false);
+  };
+
+  const fetchQuestions = async (id: number) => {
+    try {
+      const res = (await getQuestionsBySurveyId(id)) || [];
+      return res;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const handleSurvey = async (id: number) => {
+    const result = await fetchQuestions(id);
+    setQuestionsMap((prev) => ({
+      ...prev,
+      [id]: result,
+    }));
+    setActiveSurveyId(id);
   };
 
   return (
@@ -32,8 +56,29 @@ export const RenderSurveys = ({ surveys }: RenderSurveysProps) => {
           <Link href={`/surveys/${survey.id}`}>
             <h1 className='text-xl font-semibold'>{survey.survey_name}</h1>
           </Link>
+          <button
+            onClick={() => handleSurvey(survey.id!)}
+            className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition mt-2'
+          >
+            Handle Survey
+          </button>
+
+          {activeSurveyId === survey.id && (
+            <div>
+              {questionsMap[survey.id]?.length === 0 ? (
+                <button className='mt-2 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition'>
+                  Add questions
+                </button>
+              ) : (
+                <button className='mt-2 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition'>
+                  update survey
+                </button>
+              )}
+            </div>
+          )}
         </div>
       ))}
+
       <button
         onClick={() => setShowForm(!showForm)}
         className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition'
