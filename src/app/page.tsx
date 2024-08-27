5a4097105224273acd9090ac68c79d2c23cd6747
r0binsahin <robin.sahin88@gmail.com>
2024-08-27 15:44:38 +0200

    #57 renders created questions
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import useLeavePageConfirm from '@/lib/utils/use-leave-confirm';
 import { Carousel } from '../components';
-import { getQuestions } from '../lib/actions';
+import { getQuestionsBySurveyId } from '../lib/actions';
 
 export default async function Home() {
-  const questions = (await getQuestions()) || [];
+  const questions = (await getQuestionsBySurveyId(1)) || [];
 
   return (
     <main className='flex justify-center flex-col max-w-[1100px] mx-auto w-full'>
--- src/components/create-question-form.tsx
+++ src/components/create-question-form.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
-import { createQuestion } from '@/lib/actions';
+import { createQuestion, getQuestionsBySurveyId } from '@/lib/actions';
 import { Question } from '@/lib/types/Question';
 
 export const CreateQuestionForm = () => {
@@ -11,6 +11,8 @@ export const CreateQuestionForm = () => {
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const params = useParams();
+
+  const [questions, setQuestions] = useState<Question[] | []>([]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -28,6 +30,7 @@ export const CreateQuestionForm = () => {
     try {
       await createQuestion(newQuestion);
       setQuestionText('');
+      fetchQuestions();
       /*       router.push(`/surveys/${surveyId}`); // Redirect back to the survey page after submission */
     } catch (err) {
       setError('Failed to create question. Please try again.');
@@ -37,26 +40,47 @@ export const CreateQuestionForm = () => {
     }
   };
 
+  const fetchQuestions = async () => {
+    try {
+      const res = (await getQuestionsBySurveyId(+params.id)) || [];
+      setQuestions(res);
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
   return (
-    <div className='p-4 space-y-4 bg-gray-800 text-white rounded-lg shadow-md'>
-      <h2 className='text-xl font-semibold'>Add a New Question</h2>
-      {error && <p className='text-red-500'>{error}</p>}
-      <form onSubmit={handleSubmit} className='space-y-4'>
-        <textarea
-          value={questionText}
-          onChange={(e) => setQuestionText(e.target.value)}
-          placeholder='Enter your question here...'
-          required
-          className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-700 text-white'
-        />
-        <button
-          type='submit'
-          className='bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition'
-          disabled={loading}
-        >
-          {loading ? 'Submitting...' : 'Submit Question'}
-        </button>
-      </form>
-    </div>
+    <>
+      <div className='p-4 space-y-4 bg-gray-800 text-white rounded-lg shadow-md'>
+        <h2 className='text-xl font-semibold'>Add a New Question</h2>
+        {error && <p className='text-red-500'>{error}</p>}
+        <form onSubmit={handleSubmit} className='space-y-4'>
+          <textarea
+            value={questionText}
+            onChange={(e) => setQuestionText(e.target.value)}
+            placeholder='Enter your question here...'
+            required
+            className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-700 text-white'
+          />
+          <button
+            type='submit'
+            className='bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition'
+            disabled={loading}
+          >
+            {loading ? 'Submitting...' : 'Submit Question'}
+          </button>
+        </form>
+      </div>
+      <div>
+        {questions.map((q) => (
+          <h3>{q.text}</h3>
+        ))}
+      </div>
+    </>
   );
 };
