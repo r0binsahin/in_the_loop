7bcad63c11dcf810a12f5c523e762030f32173dc
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-08-28 16:08:23 +0200

    Styling fix for questions for advice cards.
    Co-authored-by: Kenny Dinh <kenny.t.dinh93@gmail.com>
    Co-authored-by: Robin Sahin <robin.sahin88@gmail.com>
    Co-authored-by: Magdalena Karpinska <magdalena.karpinska13@gmail.com>

--- src/components/questions-for-advice.tsx
+++ src/components/questions-for-advice.tsx
@@ -1,79 +1,82 @@
-"use client"
-import { getRatingsByQuestionId } from "@/lib/actions"
-import { Question } from "@/lib/types/Question"
-import { calculateAverageRatingPerQuestion } from "@/lib/utils/calculate-average-rating-per-question"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import Graph from "./graph"
-import { GraphData } from "./graph"
-import { BsChevronRight } from "react-icons/bs"
-type AverageRatings = { [key: number]: number }
-type ShowGraphStates = { [key: number]: boolean }
+"use client";
+import { getRatingsByQuestionId } from "@/lib/actions";
+import { Question } from "@/lib/types/Question";
+import { calculateAverageRatingPerQuestion } from "@/lib/utils/calculate-average-rating-per-question";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Graph from "./graph";
+import { GraphData } from "./graph";
+import { BsChevronRight } from "react-icons/bs";
+type AverageRatings = { [key: number]: number };
+type ShowGraphStates = { [key: number]: boolean };
 export const QuestionsForAdvice = ({
   questions,
   graphData,
 }: {
-  questions: Question[]
-  graphData: { [key: number]: GraphData[] }
+  questions: Question[];
+  graphData: { [key: number]: GraphData[] };
 }) => {
-  const [averageRatings, setAverageRatings] = useState<AverageRatings>({})
-  const [showGraphStates, setShowGraphStates] = useState<ShowGraphStates>({})
+  const [averageRatings, setAverageRatings] = useState<AverageRatings>({});
+  const [showGraphStates, setShowGraphStates] = useState<ShowGraphStates>({});
   useEffect(() => {
     const fetchRatings = async () => {
-      const newRatings: AverageRatings = {}
+      const newRatings: AverageRatings = {};
       for (const question of questions) {
-        const ratings = await getRatingsByQuestionId(question.id!)
+        const ratings = await getRatingsByQuestionId(question.id!);
         newRatings[question.id!] = calculateAverageRatingPerQuestion(
           ratings || []
-        )
+        );
       }
-      setAverageRatings(newRatings)
-    }
-    fetchRatings()
-  }, [questions])
+      setAverageRatings(newRatings);
+    };
+    fetchRatings();
+  }, [questions]);
   const handleShowGraph = (questionId: number) => {
     setShowGraphStates((prevStates) => ({
       ...prevStates,
       [questionId]: !prevStates[questionId],
-    }))
-  }
+    }));
+  };
   function roundToOneDecimal(number: number) {
-    return Math.round(number * 10) / 10
+    if (isNaN(number)) {
+      return "No answers yet";
+    }
+    return Math.round(number * 10) / 10;
   }
+
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 items-center py-8">
       {questions.map((q) => (
         <div
           key={q.id}
-          className="flex flex-col items-start bg-white shadow-md rounded-lg p-6 border border-gray-200 gap-3 px-2"
+          className="flex flex-col items-start p-6 border-b border-black gap-3 w-11/12"
         >
-          <h3 className="text-lg font-semibold m-2 text-start">{q.text}</h3>
-          <div className="flex items-center gap-5">
+          <h3 className="text-lg font-semibold m-2 text-center w-full">
+            {q.text}
+          </h3>
+          <div className="flex items-center gap-5 mx-auto">
             <div className="text-gray-700">
               {averageRatings[q.id!] !== undefined
-                ? roundToOneDecimal(averageRatings[q.id!])
+                ? `Average rating: ${roundToOneDecimal(averageRatings[q.id!])}`
                 : "Loading..."}
             </div>
           </div>
-          <div className="flex gap-3">
+          <div className="flex gap-3 mx-auto">
             <button
               onClick={() => handleShowGraph(q.id!)}
-              className="mt-4 bg-accent text-white py-2 px-4 rounded hover:bg-primary"
+              className="btn btn-primary text-secondary"
             >
               Show Graph
             </button>
 
             {averageRatings[q.id!] < 6 ? (
-              <Link
-                href={`/advice/${q.id}`}
-                className="mt-4 bg-primary text-white py-2 px-4 rounded hover:bg-primary"
-              >
-                <button className="flex items-center">
-                  Get Advice <BsChevronRight />
-                </button>
-              </Link>
+              <button className="btn btn-primary text-secondary">
+                <Link href={`/advice/${q.id}`} className="">
+                  Get Advice
+                </Link>
+              </button>
             ) : (
-              <div className="flex items-center text-green-600 font-bold mt-5">
+              <div className="flex items-center text-green-600 font-bold">
                 Good enough
               </div>
             )}
@@ -82,5 +85,5 @@ export const QuestionsForAdvice = ({
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
